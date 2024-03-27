4ec4d979b229d75c3bb83daaeb4043b8a4a92b82
Alden Nathan Go <aldengo99@gmail.com>
2024-03-27 19:48:52 +0800

    implement polymorphism

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < User
     rolify
     
+    has_many :schedules, as: :sectionable
+
     devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable
 
